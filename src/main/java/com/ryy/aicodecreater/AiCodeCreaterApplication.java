404d95e7efda3fd00745482d4334e271c0118ace
Richer <1592322194@qq.com>
2026-03-26 20:10:33 +0800

    基础配置

---
+++ src/main/java/com/ryy/aicodecreater/AiCodeCreaterApplication.java
@@ -0,0 +1,13 @@
+package com.ryy.aicodecreater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeCreaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodeCreaterApplication.class, args);
+	}
+
+}
