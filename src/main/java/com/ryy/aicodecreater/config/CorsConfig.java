adb4bec9139d91ae351549d9e495a61a967d7e7f
Richer <1592322194@qq.com>
2026-03-26 20:28:25 +0800

    全局跨域配置

---
+++ src/main/java/com/ryy/aicodecreater/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.ryy.aicodecreater.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * 跨域是指浏览器访问的 URL（前端地址）和后端接口地址的域名（或端口号）不一致导致的
+ * 通过全局跨域配置，让整个项目所有的接口支‍‍持跨域
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 覆盖所有请求
+        registry.addMapping("/**")
+                // 允许发送 Cookie
+                .allowCredentials(true)
+                // 放行哪些域名（必须用 patterns，否则 * 会和 allowCredentials 冲突）
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
