7339e28edce1961747994b050fbb3bbbbb1408f6
Richer <1592322194@qq.com>
2026-04-10 15:33:01 +0800

    扩展：基于 SSE 实现构建状态的实时推送，当 AI 代码生成完成后，不立即结束 SSE，而是继续： 1. 修改门面类：AiCodeGeneratorFacade.processTokenStream() 2. 改造 `VueProjectBuilder`，将构建 Vue 项目的每一步状态都推送出去到门面类，然后统一返回给前端 3. 创建一个BuildStatusMessage对象，即Vue 构建过程中的专用流式消息载体

---
+++ src/test/java/com/ryy/aicodecreater/AiCodeCreaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryy.aicodecreater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeCreaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
